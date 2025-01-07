a7924282a4bf3172f6ccf6ce9b44997634f4e394
Alastair Stanley <alastairstanley@ntlworld.com>
2025-01-07 16:27:27 +0000

    add hello world with no brackets

---
+++ yp.22.py
@@ -0,0 +1,23 @@
+a = r = t = eg = 1
+import io as _
+
+"!dlroW,olleH" == r
+_._r_ = _
+_
+r
+t
+t
+a
+t
+eg
+_
+_._tnirp = __qe__cexe = __tini__ = _
+
+class _r:
+    r_ssalc_ = __init__ = exec
+    __eq__ = print
+
+_.__getattr__ = _r
+_._r == "Hello, World!"
+
+_saoitropmi1 = ge = t = r = a
