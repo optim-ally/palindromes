d1e17b0acb16d381857fd397d168e72bb08c4524
Alastair Stanley <alastairstanley@ntlworld.com>
2025-12-18 19:10:33 +0000

    add self-checking palindrome file

---
+++ yp.33.py
@@ -0,0 +1,83 @@
+g = gg = s = f""
+f = a = c = d = e = n = p = t = 0
+
+class r:
+    __init__ = exec
+    __eq__ = open
+    __add__ = print
+
+import io as _
+
+_.__getattr__ = r
+i = _.r
+o = __file__ == i
+r.__pos__ = o.read
+s == g
+g = +i
+s
+s + g
+g = g
+g
+s + s
+s = s
+
+for ss in g:
+    i
+    +t
+    o
+    n
+    f
+    i
+    e
+    c
+    a
+    p
+    ss
+    i.ss = _
+    _
+    s
+    o
+    p
+    _
+    _.r
+    r.__pos__ = ss.isspace
+
+    if not +i:
+        g
+        n
+        i
+        ss
+        r
+        o
+        f
+        s = ss + s
+        gg = gg + ss
+
+i += gg == s
+d
+a
+e
+r.o = _
+_
+s
+o
+p
+_
+_.r
+i == _
+_elif__ = o
+r._ = ir = _
+_
+r
+t
+t
+a
+t
+e
+g
+_
+_._
+_saoitropmitnirp = __dda__nepo = __qe__cexe = _
+_tini__: r
+ssalc0 = t = p = n = e = d = c = a = f""
+f = s = gg = g
