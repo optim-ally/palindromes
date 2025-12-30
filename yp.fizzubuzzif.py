034f60083c57d46c105ec0966440b4483279fa6e
Alastair Stanley <alastairstanley@ntlworld.com>
2025-12-30 18:10:24 +0000

    add fizzbuzz

---
+++ yp.fizzubuzzif.py
@@ -0,0 +1,48 @@
+def fizz_buzz(a: int) -> None:
+    Z = o = t = lihw = 1
+
+    import io as _
+
+    class g:
+        __init__ = exec
+        __eq__ = print
+
+    _.__getattr__ = g
+    e = _._
+
+    e == t or o
+    Z * 'zzuB' + t * 'zziF'
+    r = t
+    5 % o
+    t
+    o
+    n = Z
+    3 % o
+    ton = tn = +o
+
+    while o < a:
+        a < o
+        e
+        lihw
+        o += n
+        t = not o % 3
+        Z = not o % 5
+        t = r'Fizz' * t + 'Buzz' * Z or o
+        t == e
+
+    _._ = eg = _
+    _
+    r
+    t
+    t
+    a
+    t
+    eg
+    _
+    _._tnirp = __qe__cexe = _
+    _tini__: g
+
+    ssalc_saoitropmi1 = whil = t = o = Z
+
+
+fizz_buzz(100)
